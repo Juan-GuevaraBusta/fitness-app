35568a147f5d4f88b060f02a672ceceec8b99dd8
Juan Luis Guevara Bustamante <jguevarabustamante25@gmail.com>
2025-10-02 17:38:31 -0500

    Finalized profile dtos

--- src/profiles/dto/create-profile.dto.ts
+++ src/profiles/dto/create-profile.dto.ts
@@ -1,1 +1,21 @@
-export class CreateProfileDto {}
+import { IsString, IsEmail, IsNumber, IsNotEmpty, Min, Max, IsEnum } from 'class-validator';
+import{ Transform, Type } from 'class-transformer';
+import { profileGoal, activityLevel } from '../entities/profile.entity';
+
+export class CreateProfileDto {
+    @IsEnum(profileGoal, {
+        message: 'El objetivo debe ser un objetivo valido'
+    })
+    goal: profileGoal;
+
+    @IsEnum(activityLevel, {
+        message: 'El nivel de actividad debe ser un nivel de actividad valido'
+    })
+    activityLevel: activityLevel;
+
+    @IsNumber({}, {message: 'El userId debe ser un numero'})
+    @Type(() => Number)
+    @IsNotEmpty({message: 'El userId es requerido'})
+    userId: number;
+
+}
--- src/profiles/dto/update-profile.dto.ts
+++ src/profiles/dto/update-profile.dto.ts
@@ -1,4 +1,19 @@
+import { IsEnum, IsNumber, IsNotEmpty, IsOptional } from 'class-validator';
+import { Transform, Type } from 'class-transformer';
+import { profileGoal, activityLevel } from '../entities/profile.entity';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProfileDto } from './create-profile.dto';
 
-export class UpdateProfileDto extends PartialType(CreateProfileDto) {}
+export class UpdateProfileDto extends PartialType(CreateProfileDto) {
+    @IsOptional()
+    @IsEnum(profileGoal, {
+        message: 'El objetivo debe ser un objetivo valido'
+    })
+    goal?: profileGoal;
+
+    @IsOptional()
+    @IsEnum(activityLevel, {
+        message: 'El nivel de actividad debe ser un nivel de actividad valido'
+    })
+    activityLevel?: activityLevel;
+}
